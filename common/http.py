d2ebdd1f6d78901a149ed3f4a7c0f212925fe3f0
vaskrneup <bhaskar.neupane.58@gmail.com>
2020-11-28 20:55:27 +0545

    Efficient code for user data && db fields support with pagination added in ``ResponseManager``

--- common/http.py
+++ common/http.py
@@ -2,26 +2,85 @@ from django.http import JsonResponse, HttpRequest
 from common.forms import JsonModelForm
 
 
-class ResponseManager(object):
-    def __init__(self, request: HttpRequest = None, messages: list = None, *additional_response_data):
-        self.messages = messages or []  # list of messages to be send in frontend.
+class ResponseManager:
+    def __init__(self, request: HttpRequest = None, messages: list = None,
+                 append_user_data: bool = False, **additional_response_data):
+        self.append_user_data = append_user_data  # If set True then user data is added, if set `True` additional db query must be made.
+        self.messages = messages or {
+            "error": [],  # Error message, will be displayed in red.
+            "info": [],  # Info message, will be displayed in blue.
+            "success": [],  # Success Message, will be displayed in green.
+            "warning": []  # Warning Message, will be displayed in yellow.
+        }  # list of messages to be send in frontend.
         self.request = request  # For evaluating current user condition.
+
+        if append_user_data:
+            # User related info !!
+            user_data = {
+                "is_logged_in": False,
+                "is_superuser": False
+            }
+        else:
+            user_data = {}
 
         self.response = {
             # message and error handling !!
-            "messages": {
-                "error": [],  # Error message, will be displayed in red.
-                "info": [],  # Info message, will be displayed in blue.
-                "success": [],  # Success Message, will be displayed in green.
-                "warning": []  # Warning Message, will be displayed in yellow.
-            },  # For ``non-form field`` related errors.
+            "messages": messages,  # For ``non-form field`` related errors.
             "field_errors": {},  # For ``form field`` related errors.
+            "has_errors": False,  # For Knowing if the response contains any error.
 
-            # User related info !!
-            "is_logged_in": False,
-            "is_superuser": False,
-            **additional_response_data
+            # DB/Query/Pagination Related !!
+            "pagination": {},
+            "has_pagination_data": False,
+            **user_data,
+            **additional_response_data,
         }
+
+    # For handling db query related !!
+    def add_list_view_data(self, response_field_name: str, objects: list, fields: dict):
+        """Loops through all the objects and grabs data from fields and appends to list, then add data as provided response_field_name.
+
+        @param response_field_name: adds data as this field in response data.
+        @param objects: list of objects.
+        @param fields: fields or callable, will be passed to serializer.
+        @return: None
+        """
+        self.response[response_field_name] = [obj.serialize(fields=fields) for obj in objects]  # NOQA
+
+    def add_db_data(self, response_field_name: str, object_, fields: dict):
+        """Adds object serialized data to ``response_field_name``
+
+        :param response_field_name: this will be the name of the field in response data.
+        :param object_: model object through which the query is made.
+        :param fields: fields to be included in the response data
+        :return: None
+        """
+        self.response[response_field_name] = object_.serialize(fields=fields)
+
+    def add_paginator_data(self, paginator=None, page=None):
+        """Provides support for paginator, auto adds data  !!
+
+        @param page: assigned page object of django paginator
+        @param paginator: django paginator object
+        @return: None
+        """
+        if paginator:
+            self.response["pagination"]["total_results"] = paginator.count
+
+        if page:
+            if page.has_next():
+                self.response["pagination"]["has_next_page"] = True  # NOQA
+                self.response["pagination"]["next_page_number"] = page.next_page_number()
+            else:
+                self.response["pagination"]["has_next_page"] = False  # NOQA
+
+            if page.has_previous():
+                self.response["pagination"]["has_previous_page"] = True  # NOQA
+                self.response["pagination"]["previous_page_number"] = page.previous_page_number()
+            else:
+                self.response["pagination"]["has_previous_page"] = False  # NOQA
+
+        self.response["has_pagination_data"] = True
 
     # For handling messages && notification related stuff.
     def __add_message(self, title, message, type_):
@@ -62,6 +121,15 @@ class ResponseManager(object):
             return False
 
     # For handling final data !!
+    def __add_user_data(self):
+        if self.append_user_data:
+            # for user related jobs !!
+            is_logged_in = self.request.user.is_authenticated
+            self.response["is_logged_in"] = is_logged_in
+
+            if is_logged_in:
+                self.response["is_superuser"] = self.request.user.is_superuser
+
     def compile(self, raw, *args, **kwargs):
         """
         Returns dict of response value or JsonResponse object.
@@ -71,15 +139,8 @@ class ResponseManager(object):
         :param kwargs: keyword arguments accepted by JsonResponse
         :return: `dict` of response data or JsonResponse object
         """
+        self.__add_user_data()
         self.response["has_errors"] = self.has_errors()
-
-        # for user related jobs !!
-        is_logged_in = self.request.user.is_authenticated
-        self.response["is_logged_in"] = is_logged_in
-
-        if is_logged_in:
-            self.response["is_superuser"] = self.request.user.is_superuser
-
         return self.response if raw else JsonResponse(self.response, *args, **kwargs)
 
     def __call__(self, raw=False, *args, **kwargs) -> (dict, JsonResponse):
