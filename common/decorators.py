89ff8492bb2f07a1d58bf45debc55d683ee96a62
vaskrneup <bhaskar.neupane.58@gmail.com>
2020-11-29 07:32:10 +0545

    `allow_ajax_only` && `allowed_methods` decorator added !!

--- common/decorators.py
+++ common/decorators.py
@@ -5,11 +5,12 @@ from functools import wraps
 from common.http import ResponseManager
 
 
+# User related !!
 def user_passes_test(test, failed_return_value: dict):
     """Check if the user passes the test or not, if not then return `JsonResponse` with `failed_return_value` else return `decorated function` !!
 
     :param test: Accepts function or any data type that can compare to boolean.
-    :param failed_return_value: Data to provide JsonResponse with if user doesn't pass the test.
+    :param failed_return_value: Data to JsonResponse if user doesn't pass the test.
     :return: Decorator or JsonResponse
     """
 
@@ -30,6 +31,7 @@ def user_passes_test(test, failed_return_value: dict):
     return decorator_function
 
 
+# TODO: "Change hardcoded message."
 def login_required(message=None):
     """Check if the user is logged in or not, if not then return `JsonResponse` with `message` else return `decorated function`.
 
@@ -68,6 +70,59 @@ def logout_required(message=None):
                 return res()
             else:
                 return function(request, *args, **kwargs)
+
+        return __wrapper
+
+    return decorator_function
+
+
+# Request related !!
+def allow_ajax_only(message=None):
+    """Check if the request is ajax or not, if not then return `JsonResponse` with `message` else return `decorated function`.
+
+    @param message: Message to be sent if request is not ajax, if not provided then default message(ALLOW_AJAX_ONLY_MESSAGE) will be used.
+    @return: Decorator or JsonResponse
+    """
+
+    def decorator(function):
+        @wraps(function)
+        def __wrapper(request, *args, **kwargs):
+            if not request.is_ajax():
+                res = ResponseManager()
+                res.add_warning_message(
+                    title="Not Allowed",
+                    message="Request to this page is forbidden, please make sure you use authentic app."
+                )
+                return res()
+
+            return function(request, *args, **kwargs)
+
+        return __wrapper
+
+    return decorator
+
+
+def allowed_methods(methods=None, message=None):
+    """Check if the request is ajax or not, if not then return `JsonResponse` with `message` else return `decorated function`.
+
+    @param methods: List of allowed methods, if not provided default(["GET", "POST"]) will be used.
+    @param message: Message to be sent if request is not in provided methods, if not provided then default message(ALLOWED_METHOD_MESSAGE) will be used.
+    @return: Decorator or JsonResponse
+    """
+    methods = methods or ["GET", "POST"]
+
+    def decorator_function(function):
+        @wraps(function)
+        def __wrapper(request, *args, **kwargs):
+            if request.method in methods:
+                return function(request, *args, **kwargs)
+            else:
+                res = ResponseManager()
+                res.add_warning_message(
+                    title="Method Not Allowed",
+                    message=f"""Available Methods are "{', '.join(methods)}" only"""
+                )
+                return res()
 
         return __wrapper
 
