e9396b8be0b10441b97a7b260e27064db56bd86e
vaskrneup <bhaskar.neupane.58@gmail.com>
2020-12-07 20:36:13 +0545

    file extension validator added !!

--- common/validators.py
+++ common/validators.py
@@ -1,6 +1,9 @@
 from django.core import validators
 from django.utils.deconstruct import deconstructible
 from django.utils.text import gettext_lazy as __
+from django.core.exceptions import ValidationError
+
+import os
 
 
 @deconstructible
@@ -20,3 +23,17 @@ class InternationalPhoneNumberValidator(validators.RegexValidator):
         "Enter a valid number in international or national format."
     )
     flags = 0
+
+
+def validate_file_extension(valid_file_extensions, error_message='Unsupported file extension.'):
+    """checks if file extension lies in `valid_file_extensions`."""
+
+    def _validate_file_extension(value):
+        try:
+            ext = os.path.splitext(value.name)[1]
+            if not ext.lower() in valid_file_extensions:
+                raise ValidationError(error_message)
+        except Exception as _:
+            raise ValidationError(error_message)
+
+    return _validate_file_extension
