68e431927285a78f830f6bafb3a165b7585ee806
vaskrneup <bhaskar.neupane.58@gmail.com>
2020-12-13 15:13:29 +0545

    `Can't read after request body has been read` issue fixed !!

--- common/middlewares.py
+++ common/middlewares.py
@@ -7,10 +7,11 @@ class JsonToPOSTMiddleware:
 
     def __call__(self, request):
         request.data = None
+        _body = request.body
 
         if request.method == "POST" and not request.FILES:
             try:
-                request.data = json.loads(request.body)
+                request.data = json.loads(_body)
                 if csrf_token := request.data.get("csrfmiddlewaretoken"):
                     request.POST = request.POST.copy()
                     request.POST["csrfmiddlewaretoken"] = csrf_token
